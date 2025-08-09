43b636d1d29b6ff9c9bd97280cda35db3d1f69a9
Kageyou14 <rite.yokage14@gmail.com>
2025-08-09 16:27:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 import streamlit as st
+import pandas as pd
 
 st.title("論文管理ツール")
 
-#テキスト入力欄
-post_content=st.text_input("DOIを入力してください")
+#データフレームの作成
+if "papers" not in st.session_state:
+    st.session_state.papers = pd.DataFrame(columns=["title", "authors", "journal", "year", "doi", "url"])
 
-#投稿ボタン
+#テキスト入力欄
+doi=st.text_input("DOIを入力してください")
+
+#論文追加ボタン
 if st.button("論文を追加する"):
-    if post_content:
-        #st.success("投稿完了")
-        st.text_area("投稿内容",post_content)
-    else:
-        st.warning("投稿内容を入力してください")
+    if doi:
+        #メタデータの記録
+        paper_info =  {
+        "title": "タイトル",
+        "authors": "著者",
+        "journal": "雑誌",
+        "year": "発行年",
+        "doi": doi,
+        "url": "URL"
+        }  
         
+        #データフレームに追加
+        st.session_state.papers = pd.concat(
+            [st.session_state.papers, pd.DataFrame([paper_info])],
+            ignore_index=True
+        )
+        st.success("追加完了")  
+    else:
+        st.warning("DOIを入力してください")
+          
 
 # 表の表示
 st.subheader("登録済み論文一覧")
