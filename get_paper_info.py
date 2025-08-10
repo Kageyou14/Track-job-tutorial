7240452244ce627ac0644dc3ef12291ba3ba3f04
midnq <115050051+midnq@users.noreply.github.com>
2025-08-10 10:24:25 +0900

    Update get_paper_info.py

--- get_paper_info.py
+++ get_paper_info.py
@@ -5,7 +5,7 @@ def get_paper_info_from_crossref(doi: str) -> dict | None:
     DOIを元にCrossref APIから論文のメタデータを取得
 
     Args:
-        doi (str): 論文のDOI
+        doi (str)
 
     Returns:
         dict: 成功した場合は、タイトル、著者、雑誌名、発行年を含む辞書
@@ -37,11 +37,17 @@ def get_paper_info_from_crossref(doi: str) -> dict | None:
 
         year = date_parts[0][0] if date_parts and date_parts[0] else None
         paper_url = data.get("URL", None)
+        volume = data.get("volume", None)
+        issue = data.get("issue", None)
+        pages = data.get("page", None)
         paper_info = {
             "title": title,
             "authors": authors,
             "journal": journal,
             "year": year,
+            "volume": volume, 
+            "issue": issue,   
+            "pages": pages, 
             "doi": clean_doi,
             "url": paper_url
         }
